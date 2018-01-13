0811d6b2a6f4d5c8c6702a5b61f8b459745aa8c8
pringshia <pratik@ringshia.com>
2018-01-13 10:20:36 -0500

    Add icons folder that was being left out due to global .gitignore on my system

---
+++ icons/Edit.js
@@ -0,0 +1,20 @@
+const EditIcon = ({ size = "24" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-edit-2"
+    >
+      <polygon points="16 3 21 8 8 21 3 21 3 16 16 3" />
+    </svg>
+  );
+};
+
+export default EditIcon;
