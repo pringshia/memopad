2630a140f62fc9c4d013aeef9dd81ab2030e60d0
pringshia <pratik@ringshia.com>
2018-01-13 00:22:37 -0500

    Have the time display work correctly and generate IDs for new entries

--- components/LogPad.js
+++ components/LogPad.js
@@ -8,25 +8,57 @@ export class LogPad extends React.Component {
     super(props);
   }
   state = { newEntries: [] };
+  hashCode = str => {
+    var hash = 0,
+      i,
+      chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+      chr = str.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
   handleNewEntry = contents => {
+    const currentTime = moment();
     this.setState({
       newEntries: [
         ...this.state.newEntries,
         {
           contents,
-          timestamp: moment().format("h:mm A")
+          timestamp: currentTime,
+          id: this.hashCode(currentTime.valueOf() + contents)
         }
       ]
     });
   };
   render() {
+    let last = null,
+      nodes = [];
+    this.state.newEntries.forEach((entry, i) => {
+      if (
+        last &&
+        last.timestamp.format("YYYY-MM-DD hh:mm") ===
+          entry.timestamp.format("YYYY-MM-DD hh:mm")
+      ) {
+        nodes.push(<Block content={entry.contents} key={entry.id} />);
+      } else {
+        console.log("here", i);
+        nodes.push(
+          <Block
+            timestamp={entry.timestamp.format("h:mm A")}
+            content={entry.contents}
+            key={entry.id}
+          />
+        );
+      }
+      last = entry;
+    });
+    console.log(nodes);
     return (
       <React.Fragment>
-        {this.state.newEntries.map((entry, idx) => (
-          <Block timestamp={entry.timestamp} content={entry.contents} key={idx}>
-            {/* {entry.contents} */}
-          </Block>
-        ))}
+        {nodes}
         <EntryBox onSubmit={this.handleNewEntry} />
       </React.Fragment>
     );
