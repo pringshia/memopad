a366464d9ddadc5c03c29d2c4ad79b372cd4a5d6
Pratik <pratik@ringshia.com>
2018-08-29 19:34:58 -0400

    Add authentication (only via Google for now) (#7)
    * Add very basic auth
    
    * Add in Google auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from "react";
 import LogPad from "./components/LogPad";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import firebase from "./firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 class App extends Component {
+  state = { isSignedIn: null };
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  componentDidMount() {
+    this.registerAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(user => this.setState({ isSignedIn: !!user }));
+  }
   render() {
     return (
       <div className="p-8 pt-16">
         <h1 className="title dosis text-4xl uppercase">Memopad</h1>
-        <Router>
-          <Route path="/:page?" component={LogPad} />
-        </Router>
+        {this.state.isSignedIn === null ? null : !this.state.isSignedIn ? (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        ) : (
+          <React.Fragment>
+            <a
+              className="dosis text-xs uppercase cursor-pointer logout"
+              onClick={() => firebase.auth().signOut()}
+            >
+              Log out
+            </a>
+            <Router>
+              <Route path="/:page?" component={LogPad} />
+            </Router>
+          </React.Fragment>
+        )}
       </div>
     );
   }
