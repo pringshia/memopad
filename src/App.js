9fb4fd50521c24e72c84bb3f54de26289ccbc2d6
pringshia <pratik@ringshia.com>
2018-09-11 15:06:03 -0400

    Add initial view for email auth

--- src/App.js
+++ src/App.js
@@ -55,10 +55,54 @@ class App extends Component {
                     <br />
                     made easy.
                   </h3>
-                  <StyledFirebaseAuth
-                    uiConfig={this.uiConfig}
-                    firebaseAuth={firebase.auth()}
-                  />
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <div style={{ marginTop: 20 }}>
+                      <StyledFirebaseAuth
+                        uiConfig={this.uiConfig}
+                        firebaseAuth={firebase.auth()}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        fontStyle: "italic",
+                        width: 70,
+                        textAlign: "center",
+                        color: "#888",
+                        marginTop: 20
+                      }}
+                    >
+                      &mdash; or &mdash;
+                    </div>
+
+                    <div style={{ marginLeft: 24 }}>
+                      <div style={{ fontSize: 15 }}>
+                        Have a log-in link sent to your email:
+                      </div>
+                      <input
+                        style={{
+                          fontSize: 16,
+                          border: "1px solid #999",
+                          padding: "10px 14px",
+                          borderRadius: 4
+                        }}
+                      />
+                      <button
+                        style={{
+                          border: "1px solid #0079ff",
+                          backgroundColor: "#f3fbff",
+                          color: "#0079ff",
+                          borderRadius: 4,
+                          fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+                          fontWeight: 500,
+                          fontSize: 14,
+                          padding: 11,
+                          marginLeft: 10
+                        }}
+                      >
+                        Send Email
+                      </button>
+                    </div>
+                  </div>
                 </React.Fragment>
               </Route>
               <Route
